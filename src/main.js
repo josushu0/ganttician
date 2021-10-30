caed06668e5248968efee393fea64ea0af46786e
Josué Israel Martell Castro <josue.martell17@gmail.com>
2021-10-29 19:55:53 -0500

    Added Font Awesome to the project
    Added the yarn packages neccessary to use Font Awesome icons and imported the libraries and components.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-createApp(App).use(router).mount("#app");
+library.add(faEnvelope, faLock);
+
+createApp(App).use(router).component("fa", FontAwesomeIcon).mount("#app");
