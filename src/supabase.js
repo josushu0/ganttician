6db2cf8d5ef63e23133598ad0fa780071518b17e
Josué Israel Martell Castro <josue.martell17@gmail.com>
2021-10-30 21:48:39 -0500

    Fixed supabase client export

--- src/supabase.js
+++ src/supabase.js
@@ -3,6 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://ppdxycbrqhdlyqspkcaa.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNTI5NzY4OCwiZXhwIjoxOTUwODczNjg4fQ.jYA1TgdUR-jpYo1ycyVyf_eqLryXiSFZ82zDx5NvBx0';
 
-export default {
-  supabase: createClient(supabaseUrl, supabaseAnonKey),
-};
+// eslint-disable-next-line import/prefer-default-export
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
