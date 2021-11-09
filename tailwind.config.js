e11db7104bbf6659d301c72cebb5fa695c65e055
Josué Israel Martell Castro <josue.martell17@gmail.com>
2021-11-09 11:12:58 -0600

    Added several changes to configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
+const forms = require('@tailwindcss/forms');
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        108: '27rem',
+        120: '30rem',
+      },
+      backgroundImage: {
+        'wiggle-light': "url('./src/assets/wiggle_light.svg')",
+        'wiggle-dark': "url('./src/assets/wiggle_dark.svg')",
+      },
+      colors: {
+        gray: {
+          750: '#2B3544',
+          725: '#313B4B',
+        },
+      },
+      margin: {
+        4.5: '1.05rem',
+      },
+    },
+    fontFamily: {
+      body: ['Inter'],
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundImage: ['dark'],
+    },
   },
-  plugins: [],
+  plugins: [forms],
 };
