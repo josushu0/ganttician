635fb877d48b3f2ca22f1b9384b82b758ae121c3
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-02-17 20:18:50 -0600

    feat: :sparkles: Added slideover to Gantt chart
    Added slideover to edit or create tasks, also changed some structure  of code

--- src/components/GanttChart.vue
+++ src/components/GanttChart.vue
@@ -1,37 +1,106 @@
 <template>
-  <div id="gantt"></div>
+<div>
+  <div id="gantt" class="w-screen h-full"></div>
+  <SlideOver :toggle="toggleSlideOver" @closeSlideOver="closeSlideOver" class="z-50">
+    <TaskForm @saveTask="saveTask" />
+  </SlideOver>
+</div>
 </template>
 
 <script>
 import { gantt } from 'dhtmlx-gantt';
-import { onMounted, onUnmounted, ref } from 'vue';
+import {
+  onMounted, onUnmounted, ref, reactive,
+} from 'vue';
+import SlideOver from './SlideOver.vue';
+import TaskForm from './TaskForm.vue';
 import supabase from '../supabase/supabase';
 
 export default {
   name: 'GanttChart',
-  setup() {
-    const tasks = ref();
+  components: { SlideOver, TaskForm },
+  emits: ['ganttError'],
+  setup(_, { emit }) {
+    const tasks = reactive({
+      data: {},
+      links: [],
+    });
     const session = supabase.auth.session();
     const user = supabase.auth.user().id;
 
     async function getTasks() {
       if (session) {
-        const { data: ganttTasks } = await supabase
-          .from('gantt_tasks')
-          .select('id,duration,text,start_date,progress,parent')
-          .eq('user', user);
-        tasks.value = {
-          data: ganttTasks,
-          links: [],
-        };
-        gantt.init('gantt');
-        gantt.parse(tasks.value);
+        try {
+          const { error: projectError } = await supabase
+            .from('projects')
+            .select('id')
+            .eq('user', user);
+          const { data: ganttTasks, error: tasksError } = await supabase
+            .from('gantt_tasks')
+            .select('id,duration,text,start_date,progress,parent')
+            .eq('user', user);
+            // .eq('projects', project.id);
+          tasks.data = ganttTasks;
+          tasks.links = [];
+          gantt.init('gantt');
+          gantt.parse(tasks);
+          if (projectError) {
+            throw projectError;
+          } else if (tasksError) {
+            throw tasksError;
+          }
+        } catch (error) {
+          emit('ganttError', error);
+        }
       }
+    }
+
+    // async function updateTasks() {
+    //   gantt.parse(tasks);
+    //   gantt.render();
+    // }
+
+    const toggleSlideOver = ref(false);
+    const taskId = ref();
+    function showSlideOver() {
+      toggleSlideOver.value = true;
+    }
+    function closeSlideOver() {
+      toggleSlideOver.value = false;
+    }
+    async function saveTask(taskInfo) {
+      const {
+        title, description, start, finish,
+      } = taskInfo;
+      const duration = (new Date(finish) - new Date(start)) / 1000 / 60 / 60 / 24;
+      try {
+        const { error } = await supabase
+          .from('gantt_tasks')
+          .insert([
+            {
+              user,
+              text: title,
+              description,
+              start_date: start,
+              duration,
+              progress: 0.0,
+            },
+          ]);
+        if (error) throw error;
+      } catch (error) {
+        emit('ganttError');
+      }
+      // updateTasks();
+      const task = gantt.getTask(taskId.value);
+      gantt.deleteTask(task.id);
+      gantt.hideLightbox();
+      taskId.value = null;
     }
 
     onMounted(() => {
       gantt.i18n.setLocale('es');
       gantt.config.xml_date = '%Y-%m-%d';
+      gantt.config.cascade_delete = false;
 
       // Scales configuration
       gantt.config.scale_height = 50;
@@ -90,31 +159,42 @@ export default {
       gantt.config.touch = 'force';
 
       // Lightbox configuration
-      gantt.locale.labels.section_time = 'Periodo';
-      gantt.config.lightbox.sections = [
-        {
-          name: 'description',
-          height: 45,
-          type: 'textarea',
-          map_to: 'text',
-          focus: true,
-        },
-        {
-          name: 'time',
-          height: 35,
-          type: 'duration',
-          map_to: 'auto',
-        },
-      ];
-      gantt.config.lightbox_additional_height = 110;
-      gantt.config.buttons_left = ['gantt_cancel_btn'];
-      gantt.config.buttons_right = ['gantt_save_btn', 'gantt_delete_btn'];
+      // gantt.locale.labels.section_time = 'Periodo';
+      // gantt.config.lightbox.sections = [
+      //   {
+      //     name: 'description',
+      //     height: 45,
+      //     type: 'textarea',
+      //     map_to: 'text',
+      //     focus: true,
+      //   },
+      //   {
+      //     name: 'time',
+      //     height: 35,
+      //     type: 'duration',
+      //     map_to: 'auto',
+      //   },
+      // ];
+      // gantt.config.lightbox_additional_height = 110;
+      // gantt.config.buttons_left = ['gantt_cancel_btn'];
+      // gantt.config.buttons_right = ['gantt_save_btn', 'gantt_delete_btn'];
+
+      gantt.showLightbox = (id) => {
+        showSlideOver();
+        taskId.value = id;
+      };
+
+      gantt.hideLightbox = () => {
+        closeSlideOver();
+        gantt.refreshData();
+      };
+
+      // gantt.attachEvent('on', () => {});
 
       // Plugins configuration
       gantt.plugins({
         drag_timeline: true,
         tooltip: true,
-        undo: true,
       });
       gantt.templates.tooltip_text = (start, end) => `<b>Inicio:</b> ${gantt.templates
         .tooltip_date_format(start)}<br/><b>Fin:</b> ${gantt.templates.tooltip_date_format(end)}`;
@@ -122,9 +202,17 @@ export default {
     });
 
     onUnmounted(() => {
-      tasks.value = null;
+      tasks.data = null;
+      tasks.links = null;
       gantt.clearAll();
     });
+
+    return {
+      toggleSlideOver,
+      showSlideOver,
+      closeSlideOver,
+      saveTask,
+    };
   },
 };
 </script>
