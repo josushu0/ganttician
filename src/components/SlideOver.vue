94df85ea73d6a11cab73a12e0dcd0c8ea277f369
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-02-17 20:15:53 -0600

    feat: :lipstick: Changed styles and added slot
    Changed styles from overlay and added a slot for the task form

--- src/components/SlideOver.vue
+++ src/components/SlideOver.vue
@@ -6,7 +6,8 @@
         <TransitionChild as="template"
         enter="ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100"
         leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
-          <DialogOverlay class="absolute inset-0 bg-gray-500/75 transition-opacity" />
+          <DialogOverlay class="absolute inset-0 bg-gray-500/75 dark:bg-gray-800/60
+          transition-opacity" />
         </TransitionChild>
         <div class="fixed inset-y-0 right-0 pl-10 max-w-full flex">
           <TransitionChild as="template"
@@ -21,25 +22,27 @@
                 <div class="absolute top-0 left-0 -ml-8 pt-4 pr-2 flex sm:-ml-10 sm:pr-4">
                   <button type="button"
                   class="rounded-md text-gray-300 hover:text-white
-                  focus:outline-none focus:ring-2 focus:ring-white
-                  dark:text-gray-700 dark:hover:text-gray-900 dark:focus:ring-gray-800"
+                  focus:outline-none focus:ring-2 focus:ring-white"
                   @click="close">
                     <span class="sr-only">Cerrar panel</span>
                     <XIcon class="h-6 w-6" aria-hidden="true" />
                   </button>
                 </div>
               </TransitionChild>
-              <div class="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll
-              dark:bg-gray-700 ">
+              <div class="h-full flex flex-col py-6 bg-gray-50 shadow-xl overflow-y-scroll
+              dark:bg-gray-800 ">
                 <div class="px-4 sm:px-6">
-                  <DialogTitle class="text-lg font-medium text-gray-900 dark:text-gray-200">
+                  <DialogTitle class="text-xl font-medium text-gray-700 dark:text-gray-200
+                  border-b-2 border-gray-700 dark:border-gray-200 pb-2">
                     Editar tarea
                   </DialogTitle>
                 </div>
                 <div class="mt-6 relative flex-1 px-4 sm:px-6">
                   <!-- Replace with your content -->
                   <div class="absolute inset-0 px-4 sm:px-6">
-                    <div class="h-full border-2 border-dashed border-gray-200" aria-hidden="true" />
+                    <div class="h-full" aria-hidden="true">
+                      <slot></slot>
+                    </div>
                   </div>
                   <!-- /End replace -->
                 </div>
