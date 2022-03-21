292e0cfae68d14860a81c8f58f3aafac59928667
MiguelAngelGM99 <gm.miguelangel@hotmail.com>
2022-03-21 12:51:24 -0600

    Co-authored-by: Josué Israel Martell Castro <josue.martell17@gmail.com>

---
+++ src/components/Proyect.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="grid gap-0 grid-rows-[52px,calc(100vh-52px)] h-screen w-screen
+  bg-gray-100 dark:bg-gray-700 lg:grid-cols-[3.5rem,calc(100vw-3.5rem)]">
+    <TransitionRoot
+      :show="toggleAlert"
+      enter="transition-opacity duration-75"
+      enter-from="opacity-0"
+      enter-to="opacity-100"
+      leave="transition-opacity duration-150"
+      leave-from="opacity-100"
+      leave-to="opacity-0"
+    >
+      <AlertDialog :type='alertType' :description='alertDescription'
+      class="absolute right-2 top-2" />
+    </TransitionRoot>
+    <div v-for='project in projects' :key='project.id'>
+      <button class="navbar_item rounded-lg ml-2 lg:mt-2 lg:ml-0 lg:w-11 lg:h-11
+                  flex justify-center items-center w-10 h-10 shadow-lg"
+                  @click="showProjects">
+                  {{project.project_name}}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeMount, ref } from 'vue';
+import { TransitionRoot } from '@headlessui/vue';
+import AlertDialog from './AlertDialog.vue';
+import supabase from '../supabase/supabase';
+
+const session = supabase.auth.session();
+const toggleAlert = ref(false);
+const alertType = ref();
+const alertDescription = ref();
+
+// eslint-disable-next-line no-undef
+const emit = defineEmits(['showProjects']);
+let showGantt = false;
+const projects = ref('');
+
+onBeforeMount(async () => {
+  try {
+    const { data, error } = await supabase
+      .from('projects')
+      .select('*')
+      .eq('user', session.user.id);
+
+    if (error) {
+      throw error;
+    }
+    if (data) {
+      projects.value = data;
+    }
+  } catch (error) {
+    alert(error);
+  }
+});
+
+const showProjects = () => {
+  showGantt = !showGantt;
+  emit('showProjects', showGantt);
+};
+
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,27 +1,34 @@
 <template>
-  <div class="grid gap-0 grid-rows-[52px,calc(100vh-52px)] h-screen w-screen
-  bg-gray-100 dark:bg-gray-700 lg:grid-cols-[3.5rem,calc(100vw-3.5rem)]"
-  v-if="session">
-    <TransitionRoot
-      :show="toggleAlert"
-      enter="transition-opacity duration-75"
-      enter-from="opacity-0"
-      enter-to="opacity-100"
-      leave="transition-opacity duration-150"
-      leave-from="opacity-100"
-      leave-to="opacity-0"
-    >
-      <AlertDialog :type='alertType' :description='alertDescription'
-      class="absolute right-2 top-2" />
-    </TransitionRoot>
-    <Navbar @hideDrawer="hideDrawer" @showProjects="showSlideOver" @signoutError="signoutError"
-    class="row-start-1 col-start-1 w-screen h-[52px]
-    lg:col-start-1 lg:row-start-1 lg:w-14 lg:h-screen" />
-    <GanttChart
-    class="row-start-2 col-start-1 w-screen h-full overflow-hidden
-    lg:col-start-2 lg:row-start-1 lg:w-full lg:h-screen"
-    @ganttError="ganttError" />
-    <SlideOver :toggle="toggleSlideOver" @closeSlideOver="closeSlideOver" class="z-50"/>
+  <div>
+  <div v-if='project'>
+    <div class="grid gap-0 grid-rows-[52px,calc(100vh-52px)] h-screen w-screen
+    bg-gray-100 dark:bg-gray-700 lg:grid-cols-[3.5rem,calc(100vw-3.5rem)]"
+    v-if="session">
+      <TransitionRoot
+        :show="toggleAlert"
+        enter="transition-opacity duration-75"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="transition-opacity duration-150"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <AlertDialog :type='alertType' :description='alertDescription'
+        class="absolute right-2 top-2" />
+      </TransitionRoot>
+      <Navbar @hideDrawer="hideDrawer" @showProjects="showSlideOver" @signoutError="signoutError"
+      class="row-start-1 col-start-1 w-screen h-[52px]
+      lg:col-start-1 lg:row-start-1 lg:w-14 lg:h-screen" />
+      <GanttChart
+      class="row-start-2 col-start-1 w-screen h-full overflow-hidden
+      lg:col-start-2 lg:row-start-1 lg:w-full lg:h-screen"
+      @ganttError="ganttError" />
+      <SlideOver :toggle="toggleSlideOver" @closeSlideOver="closeSlideOver" class="z-50"/>
+    </div>
+  </div>
+  <div v-else>
+    <Project />
+  </div>
   </div>
 </template>
 
@@ -34,7 +41,10 @@ import Navbar from '../components/Navbar.vue';
 import GanttChart from '../components/GanttChart.vue';
 import SlideOver from '../components/SlideOver.vue';
 import AlertDialog from '../components/AlertDialog.vue';
+import Project from '../components/Proyect.vue';
 import supabase from '../supabase/supabase';
+
+const project = ref(false);
 
 const router = useRouter();
 const session = supabase.auth.session();
