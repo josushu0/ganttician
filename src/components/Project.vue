6cca18827457a1c2fcd136c04ce2f9d11a1c407c
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-03-30 17:59:35 -0600

    feat: sparkles: Added a title to projects page

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="bg-gray-100 dark:bg-gray-700 h-screen w-screen grid place-content-center">
-    <div v-if="projects" class="flex justify-center items-center flex-wrap gap-3 max-w-3xl m-auto">
-      <button v-for='projectItem in projects' :key='projectItem.id'
-        class="bg-gray-300 text-gray-900 rounded-lg h-32 w-32 shadow-lg
-          dark:bg-gray-800 dark:text-gray-50 hover:bg-gray-400 hover:dark:bg-gray-900"
-          @click="getProject(projectItem)">
-          {{projectItem.project_name}}
-      </button>
-      <button class="bg-gray-300 text-gray-900 rounded-lg h-32 w-32 shadow-lg
-        dark:bg-gray-800 dark:text-gray-50 flex justify-center items-center
-        hover:bg-gray-400 hover:dark:bg-gray-900">
-        <PlusSmIcon class="h-12 w-12" />
-      </button>
+  <div class="bg-gray-100 dark:bg-gray-700 h-screen w-screen
+    grid place-content-center">
+    <div v-if="projects" class="w-screen">
+      <h1 class="flex justify-center mb-10 text-4xl text-gray-900 dark:text-gray-50 underline">
+        Proyectos
+      </h1>
+      <div class="flex justify-center items-center flex-wrap gap-3 max-w-3xl m-auto">
+        <button v-for='projectItem in projects' :key='projectItem.id'
+          class="bg-gray-300 text-gray-900 rounded-lg h-32 w-32 shadow-lg
+            dark:bg-gray-800 dark:text-gray-50 hover:bg-gray-400 hover:dark:bg-gray-900"
+            @click="getProject(projectItem)">
+            {{projectItem.project_name}}
+        </button>
+        <button class="bg-gray-300 text-gray-900 rounded-lg h-32 w-32 shadow-lg
+          dark:bg-gray-800 dark:text-gray-50 flex justify-center items-center
+          hover:bg-gray-400 hover:dark:bg-gray-900">
+          <PlusSmIcon class="h-12 w-12" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
