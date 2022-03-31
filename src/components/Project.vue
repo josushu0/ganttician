d6e5f9162f4a9e9e2f81744855282b199353bbcd
Josué Israel Martell Castro <josue.martell17@gmail.com>
2022-03-30 19:37:01 -0600

    feat: :sparkles: Implemented modal to create new projects
    Implemented new modal to create new projects which are added to the list and to the database.

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -14,17 +14,23 @@
         </button>
         <button class="bg-gray-300 text-gray-900 rounded-lg h-32 w-32 shadow-lg
           dark:bg-gray-800 dark:text-gray-50 flex justify-center items-center
-          hover:bg-gray-400 hover:dark:bg-gray-900">
+          hover:bg-gray-400 hover:dark:bg-gray-900" @click="showModal" >
           <PlusSmIcon class="h-12 w-12" />
         </button>
       </div>
     </div>
+    <Modal :toggle="toggleModal" @closeModal="closeModal">
+      <ProjectForm @addProject="addProject"/>
+    </Modal>
   </div>
 </template>
 
 <script setup>
 import { onBeforeMount, ref } from 'vue';
 import { PlusSmIcon } from '@heroicons/vue/outline';
+import { v4 as uuid } from 'uuid';
+import Modal from './Modal.vue';
+import ProjectForm from './ProjectForm.vue';
 import useProjectStore from '../stores/projectStore';
 import supabase from '../supabase/supabase';
 
@@ -39,7 +45,7 @@ onBeforeMount(async () => {
   try {
     const { data, error } = await supabase
       .from('projects')
-      .select('id,project_name')
+      .select('*')
       .eq('user', session.user.id);
 
     if (error) throw error;
@@ -53,6 +59,16 @@ const showGantt = () => {
   emit('showGantt');
 };
 
+const toggleModal = ref(false);
+
+function showModal() {
+  toggleModal.value = true;
+}
+
+function closeModal() {
+  toggleModal.value = false;
+}
+
 const getProject = (projectItem) => {
   projectStore.project = projectItem;
   if (localStorage.getItem('projectId') === null && localStorage.getItem('projectName') === null) {
@@ -60,6 +76,24 @@ const getProject = (projectItem) => {
     localStorage.setItem('projectName', projectItem.project_name);
   }
   showGantt();
+};
+
+const addProject = async (projectInfo) => {
+  const id = uuid();
+  try {
+    const { error } = await supabase
+      .from('projects')
+      .insert({
+        id,
+        project_name: projectInfo.project_name.value,
+        user: session.user.id,
+      });
+    projects.value.push(projectInfo);
+    closeModal();
+    if (error) throw error;
+  } catch (error) {
+    emit('projectError', error);
+  }
 };
 
 </script>
---
+++ src/components/ProjectForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full">
+    <DialogTitle as="h1" class="text-2xl font-medium text-gray-700 dark:text-gray-200 mb-5">
+      Crear proyecto
+    </DialogTitle>
+    <form @submit.prevent class="text-gray-700 dark:text-gray-200 w-full">
+      <label for="name" class="font-medium">Nombre del proyecto</label>
+      <input type="text" name="name" id="name" class="rounded border-gray-300 w-full
+        focus:ring-purple-600 focus:border-transparent text-sm sm:text-base
+        dark:bg-gray-700 dark:border-[#515c6d] dark:focus:ring-purple-500 mt-1"
+        v-model="projectName">
+      <button @click="addProject"
+              class="rounded bg-purple-500 text-white py-2 px-6 mt-4
+      hover:bg-purple-600">
+        Guardar
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { DialogTitle } from '@headlessui/vue';
+import { ref } from 'vue';
+
+// eslint-disable-next-line no-undef
+const emit = defineEmits(['addProject']);
+const projectName = ref('');
+const projectInfo = {
+  project_name: projectName,
+};
+
+const addProject = () => {
+  emit('addProject', projectInfo);
+};
+</script>
