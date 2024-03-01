e35c0079a238d4e682391e7834861bc1b90b3915
Josué Martell <josue.martell17@gmail.com>
2024-03-01 16:36:16 -0600

    feat: added tailwind and color mode support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+	devtools: { enabled: true },
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@nuxtjs/color-mode'
+	],
+	tailwindcss: {
+		config: {
+			darkMode: 'class'
+		}
+	},
+	colorMode: {
+		classSuffix: ''
+	}
 })
