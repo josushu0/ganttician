40c16db4d800adda7402a049741b5abe1b1f5bb0
Josué Martell <josue.martell17@gmail.com>
2024-03-08 14:48:41 -0600

    style: formatting

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,4 +1,4 @@
 <template>
-		<div class="size-14 bg-primary-light dark:bg-primary-dark rounded-full border-2 border-neutral-dark" />
+	<div
+		class="size-14 bg-primary-light dark:bg-primary-dark rounded-full border-2 border-neutral-dark" />
 </template>
-
--- components/LogoComplete.vue
+++ components/LogoComplete.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="flex items-center gap-2">
 		<Logo />
-		<h1 class="text-4xl text-text-light font-bold dark:text-text-dark">Ganttician</h1>
+		<h1 class="text-4xl text-text-light font-bold dark:text-text-dark">
+			Ganttician
+		</h1>
 	</div>
 </template>
