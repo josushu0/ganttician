072145e5f19e7ad2f0f8439f0faebb92ae98dc37
Josué Martell <josue.martell17@gmail.com>
2024-03-19 15:06:01 -0600

    feat: created ui to select organization

---
+++ components/NewProjectButton.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+	<UButton label="New project" color="primary" />
+</template>
---
+++ components/OrgSelect.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const organizations = [
+	{ id: 1, name: 'Org 1' },
+	{ id: 2, name: 'Org 2' },
+	{ id: 3, name: 'Org 3' },
+]
+const selected = ref(organizations[0])
+</script>
+
+<template>
+	<USelectMenu
+		searchable
+		searchable-placeholder="Find organization..."
+		option-attribute="name"
+		by="id"
+		:search-attributes="['name']"
+		v-model="selected"
+		:options="organizations" />
+</template>
