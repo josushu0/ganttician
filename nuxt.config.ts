b27e14c88a137a7b6518339b55b7cabe7459bb55
Josué Martell <josue.martell17@gmail.com>
2024-03-20 19:38:19 -0600

    feat: added pinia for state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@vueuse/nuxt', '@nuxt/ui'],
+	modules: ['@vueuse/nuxt', '@nuxt/ui', '@pinia/nuxt'],
 	ui: {
 		icons: ['simple-icons'],
 	},
 })
+
