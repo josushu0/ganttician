411436e3992d8d3c97e65d7416fdef62e5d1fe73
Josué Martell <josue.martell17@gmail.com>
2024-03-21 23:39:59 -0600

    feat: created user menu dropdown component

---
+++ components/UserDropdown.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { LogOut } from 'lucide-vue-next'
+
+const userStore = useUserStore()
+const isLargeScreen = useMediaQuery('(min-width: 1280px)')
+</script>
+
+<template>
+	<DropdownMenu>
+		<DropdownMenuTrigger>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger as-child>
+						<Button size="icon" variant="ghost">
+							<Avatar>
+								<AvatarImage
+									:src="userStore.user?.avatar!"
+									:alt="userStore.user?.username" />
+								<AvatarFallback
+									:text="userStore.user?.username"
+									class="truncate" />
+							</Avatar>
+							<span class="sr-only">User menu</span>
+						</Button>
+					</TooltipTrigger>
+					<TooltipContent
+						:side="isLargeScreen ? 'right' : 'top'"
+						:sideOffset="14">
+						User menu
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</DropdownMenuTrigger>
+		<DropdownMenuContent
+			:side="isLargeScreen ? 'right' : 'top'"
+			:sideOffset="14"
+			class="w-fit h-fit p-2">
+			<DropdownMenuItem @click="userStore.logout" variant="ghost">
+				<LogOut class="size-4 me-2" />
+				Logout
+			</DropdownMenuItem>
+		</DropdownMenuContent>
+	</DropdownMenu>
+</template>
