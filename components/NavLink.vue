ba08aa4a839780bfed129560a848be2c64b10026
Josué Martell <josue.martell17@gmail.com>
2024-03-21 23:40:14 -0600

    feat: created nav link component

---
+++ components/NavLink.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const isLargeScreen = useMediaQuery('(min-width: 1280px)')
+const props = defineProps<{
+	condition: boolean
+	to: string
+	popover: string
+}>()
+</script>
+
+<template>
+	<TooltipProvider v-if="props.condition">
+		<Tooltip>
+			<TooltipTrigger as-child>
+				<Button size="icon" variant="ghost">
+					<NuxtLink :to="props.to">
+						<slot />
+					</NuxtLink>
+				</Button>
+			</TooltipTrigger>
+			<TooltipContent :side="isLargeScreen ? 'right' : 'top'" :sideOffset="14">
+				{{ props.popover }}
+			</TooltipContent>
+		</Tooltip>
+	</TooltipProvider>
+</template>
