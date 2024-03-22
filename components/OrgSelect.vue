e0ede6176bce1a18fa6e099e8f55602bf375e7bb
Josué Martell <josue.martell17@gmail.com>
2024-03-22 00:20:18 -0600

    refactor: refactored organization select

--- components/OrgSelect.vue
+++ components/OrgSelect.vue
@@ -1,15 +1,58 @@
 <script setup lang="ts">
+import { Check, ChevronsUpDown } from 'lucide-vue-next'
+
+import { cn } from '@/lib/utils'
+import type { Organization } from '~/db/schema/organization'
+
+const open = ref(false)
 const orgStore = useOrganizationsStore()
-await orgStore.fetchOrganizations()
+if (orgStore.organizations.length === 0) {
+	await orgStore.fetchOrganizations()
+}
+
+const selectOrg = (value: Organization) => {
+	orgStore.selectedOrganization = value
+	open.value = false
+}
 </script>
 
 <template>
-	<USelectMenu
-		searchable
-		searchable-placeholder="Find organization..."
-		option-attribute="name"
-		by="id"
-		:search-attributes="['name']"
-		v-model="orgStore.selectedOrganization"
-		:options="orgStore.organizations" />
+	<Popover v-model:open="open">
+		<PopoverTrigger as-child>
+			<Button
+				variant="outline"
+				role="combobox"
+				:aria-expanded="open"
+				class="justify-between">
+				{{ orgStore.selectedOrganization?.name || 'Select an Organization...' }}
+				<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+			</Button>
+		</PopoverTrigger>
+		<PopoverContent class="p-0" align="start">
+			<Command>
+				<CommandInput placeholder="Search Organization" />
+				<CommandEmpty>No Organization found.</CommandEmpty>
+				<CommandList>
+					<CommandGroup>
+						<CommandItem
+							v-for="organization in orgStore.organizations"
+							:key="organization.id"
+							:value="organization.name"
+							@select="selectOrg(organization)">
+							{{ organization.name }}
+							<Check
+								:class="
+									cn(
+										'ml-auto h-4 w-4',
+										orgStore.selectedOrganization.id === organization.id
+											? 'opacity-100'
+											: 'opacity-0',
+									)
+								" />
+						</CommandItem>
+					</CommandGroup>
+				</CommandList>
+			</Command>
+		</PopoverContent>
+	</Popover>
 </template>
