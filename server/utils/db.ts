8f0fdf4b2d5b14470f06c93857a36df6fb8cecda
Josué Martell <josue.martell17@gmail.com>
2024-04-03 17:56:33 -0600

    feat: set schemas in db initialization

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
+import * as org from '@/db/schema/organization'
+import * as pro from '@/db/schema/projects'
+import * as aut from '@/db/schema/auth'
 
 const client = createClient({
 	url: process.env.DATABASE_URL!,
 	authToken: process.env.DATABASE_AUTH_TOKEN,
 })
-export const db = drizzle(client)
+export const db = drizzle(client, { schema: { ...org, ...pro, ...aut } })
