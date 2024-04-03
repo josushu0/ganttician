d90c06ad40e98930ad352bb1b972674b3d387400
Josué Martell <josue.martell17@gmail.com>
2024-04-03 16:02:00 -0600

    refactor: reversed if to catch all routes

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware((to, _) => {
 	const userStore = useUserStore()
 	if (userStore.user && (to.path === '/signin' || to.path === '/')) {
 		return navigateTo('/dashboard')
-	} else if (!userStore.user && (to.path === '/dashboard' || to.path === '/')) {
+	} else if (!userStore.user && to.path !== '/signin' && to.path !== '/') {
 		return navigateTo('/signin')
 	}
 })
