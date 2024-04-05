d4afdbcf32c44d283e56f05c588b78d8d871f075
Josué Martell <josue.martell17@gmail.com>
2024-04-04 21:53:20 -0600

    fix: redirects when not authenticated

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware((to, _) => {
 	const userStore = useUserStore()
 	if (userStore.user && (to.path === '/signin' || to.path === '/')) {
 		return navigateTo('/dashboard')
-	} else if (!userStore.user && to.path !== '/signin' && to.path !== '/') {
+	} else if (!userStore.user && to.path !== '/signin') {
 		return navigateTo('/signin')
 	}
 })
