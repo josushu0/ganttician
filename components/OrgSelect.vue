bf63f0d8964b88ed2d705b07814e0ec7de228a60
Josué Martell <josue.martell17@gmail.com>
2024-04-29 18:05:25 -0600

    refactor: remade org selector with radix components

--- components/OrgSelect.vue
+++ components/OrgSelect.vue
@@ -1,60 +1,73 @@
 <script setup lang="ts">
-import { Check, ChevronsUpDown } from 'lucide-vue-next'
-import { cn } from '@/lib/utils'
+import { Icon } from '@iconify/vue'
+import { SelectContent, SelectItemText } from 'radix-vue'
 import type { Organization } from '~/db/schema/organization'
 
-const { orgs } = defineProps<{
-	orgs: Organization[]
-}>()
+const emit = defineEmits(['selectedOrg'])
+
+const userStore = useUserStore()
+const { data, pending } = await useFetch<Organization[]>('/api/organizations', {
+	query: { userId: userStore.user?.id },
+})
 
 const orgStore = useOrganizationsStore()
-orgStore.organizations = orgs
-orgStore.selectedOrganization = orgStore.organizations[0]
+orgStore.selectedOrganization = data.value ? data.value[0].id : ''
 
-const open = ref(false)
-const selectOrg = (value: Organization) => {
-	orgStore.selectedOrganization = value
-	open.value = false
-}
+onMounted(() => {
+	emit('selectedOrg')
+})
+
+watch(orgStore, () => {
+	emit('selectedOrg')
+})
 </script>
 
 <template>
-	<Popover v-model:open="open">
-		<PopoverTrigger as-child>
-			<Button
-				variant="outline"
-				role="combobox"
-				:aria-expanded="open"
-				class="justify-between">
-				{{ orgStore.selectedOrganization?.name || 'Select an Organization...' }}
-				<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-			</Button>
-		</PopoverTrigger>
-		<PopoverContent class="p-0" align="start">
-			<Command>
-				<CommandInput placeholder="Search Organization" />
-				<CommandEmpty>No Organization found.</CommandEmpty>
-				<CommandList>
-					<CommandGroup>
-						<CommandItem
-							v-for="organization in orgs"
-							:key="organization.id"
-							:value="organization.name"
-							@select="selectOrg(organization)">
-							{{ organization.name }}
-							<Check
-								:class="
-									cn(
-										'ml-auto h-4 w-4',
-										orgStore.selectedOrganization.id === organization.id
-											? 'opacity-100'
-											: 'opacity-0',
-									)
-								" />
-						</CommandItem>
-					</CommandGroup>
-				</CommandList>
-			</Command>
-		</PopoverContent>
-	</Popover>
+	<SelectRoot v-model="orgStore.selectedOrganization">
+		<SelectTrigger
+			aria-label="Select an organization"
+			class="flex items-center justify-between gap-2 p-2 max-w-52 w-full rounded bg-background border-2 border-border outline-primary focus-visible:outline">
+			<SelectValue placeholder="Select an organization" class="truncate" />
+			<Icon icon="lucide:chevron-down" class="size-5 shrink-0" />
+		</SelectTrigger>
+		<SelectPortal>
+			<SelectContent
+				position="popper"
+				side="bottom"
+				:side-offset="3"
+				class="bg-background border-2 border-border p-1 rounded content">
+				<SelectScrollUpButton
+					class="flex items-center justify-center cursor-default">
+					<Icon icon="lucide:chevron-up" />
+				</SelectScrollUpButton>
+				<SelectViewport>
+					<SelectLabel class="text-foreground px-2 font-bold">
+						Select Organization
+					</SelectLabel>
+					<SelectSeparator class="h-px bg-border m-2"> </SelectSeparator>
+					<SelectGroup v-if="!pending">
+						<SelectItem
+							v-for="(org, index) in data"
+							:key="index"
+							:value="org.id"
+							class="flex items-center py-1 px-2 rounded text-foreground hover:bg-primary hover:text-primary-foreground relative select-none transition-colors focus-visible:bg-primary focus-visible:text-primary-foreground outline-none">
+							<SelectItemIndicator
+								class="absolute left-0 w-6 inline-flex justify-center items-center">
+								<Icon icon="lucide:check" />
+							</SelectItemIndicator>
+							<SelectItemText class="pl-4">
+								{{ org.name }}
+							</SelectItemText>
+						</SelectItem>
+					</SelectGroup>
+				</SelectViewport>
+			</SelectContent>
+		</SelectPortal>
+	</SelectRoot>
 </template>
+
+<style>
+.content {
+	width: var(--radix-select-trigger-width);
+}
+</style>
--- stores/organizations.ts
+++ stores/organizations.ts
@@ -1,8 +1,5 @@
-import type { Organization } from '~/db/schema/organization'
-
 export const useOrganizationsStore = defineStore('organizations', {
 	state: () => ({
-		organizations: [] as Organization[],
-		selectedOrganization: {} as Organization,
+		selectedOrganization: '',
 	}),
 })
