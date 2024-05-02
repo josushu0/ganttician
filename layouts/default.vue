fba9f0594d694e9037db273089368bc32a61f84e
Josué Martell <josue.martell17@gmail.com>
2024-05-01 20:59:47 -0600

    feat: setup sonner toast

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Toaster } from 'vue-sonner'
+
+const isDark = useDark()
+</script>
 
 <template>
 	<div class="w-screen h-dvh flex flex-col-reverse xl:flex-row">
 		<Navbar />
 		<slot />
+		<Toaster
+			position="top-center"
+			:expand="true"
+			close-button
+			:theme="isDark ? 'dark' : 'light'" />
 	</div>
 </template>
