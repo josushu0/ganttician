1a9bee55226e81aab591e400ce58407f4222eefa
Josué Martell <josue.martell17@gmail.com>
2024-05-01 21:00:21 -0600

    style: add padding to new org button

--- components/NewOrgButton.vue
+++ components/NewOrgButton.vue
@@ -1,7 +1,7 @@
 <template>
 	<NuxtLink
 		to="/organization/new"
-		class="bg-primary p-2 rounded text-primary-foreground truncate outline-primary outline-offset-2 focus-visible:outline">
+		class="bg-primary py-2 px-4 rounded text-primary-foreground truncate outline-primary outline-offset-2 focus-visible:outline">
 		New organization
 	</NuxtLink>
 </template>
