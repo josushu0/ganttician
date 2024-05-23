ef0ce7c91655439e7d149044fc1fb01ab897d261
Josué Martell <josue.martell17@gmail.com>
2024-05-22 18:57:30 -0600

    feat: added columns and tasks schemas to db config

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -3,9 +3,13 @@ import { drizzle } from 'drizzle-orm/libsql'
 import * as org from '@/db/schema/organization'
 import * as pro from '@/db/schema/projects'
 import * as aut from '@/db/schema/auth'
+import * as col from '@/db/schema/columns'
+import * as tas from '@/db/schema/tasks'
 
 const client = createClient({
 	url: process.env.DATABASE_URL!,
 	authToken: process.env.DATABASE_AUTH_TOKEN,
 })
-export const db = drizzle(client, { schema: { ...org, ...pro, ...aut } })
+export const db = drizzle(client, {
+	schema: { ...org, ...pro, ...aut, ...col, ...tas },
+})
