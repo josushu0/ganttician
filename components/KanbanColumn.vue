73ad0b13953833b68c08f75cf7f9b3c747d9eb4f
Josué Martell <josue.martell17@gmail.com>
2024-05-23 13:05:56 -0600

    revert: dont reorder columns by drag n drop
    Refs: 02369207

--- components/KanbanColumn.vue
+++ components/KanbanColumn.vue
@@ -37,26 +37,13 @@ const insertAbove = (mouseY: number) => {
 
 	return closestTask
 }
-
-const dragging = ref(false)
-const dragStart = () => {
-	dragging.value = true
-}
-
-const dragEnd = () => {
-	dragging.value = false
-}
 </script>
 
 <template>
 	<div
 		ref="column"
-		draggable="true"
 		@dragover="dragOver"
-		@dragstart="dragStart"
-		@dragend="dragEnd"
-		class="rounded bg-background border-2 border-border p-4 w-64 h-min space-y-2 min-w-64 select-none column"
-		:class="dragging && 'dragging_col'">
+		class="rounded bg-background border-2 border-border p-4 w-64 h-min space-y-2 min-w-64">
 		<h1 class="text-xl font-bold">{{ $props.column.name }}</h1>
 		<slot />
 	</div>
--- pages/project/[id]/index.vue
+++ pages/project/[id]/index.vue
@@ -29,39 +29,6 @@ const { data } = await useFetch<Data[]>('/api/tasks', {
 	method: 'get',
 	query: { id: id },
 })
-
-const board = ref<HTMLElement | null>(null)
-
-const dragOver = (e: DragEvent) => {
-	e.preventDefault()
-	const leftColumn = insertLeft(e.clientX)
-	const currentColumn = document.querySelector('.dragging_col')
-	if (!leftColumn) {
-		board.value?.appendChild(currentColumn!)
-	} else {
-		board.value?.insertBefore(currentColumn!, leftColumn)
-	}
-}
-
-const insertLeft = (mouseX: number) => {
-	const columns = board.value?.querySelectorAll('.column:not(.dragging_col)')
-
-	let closestTask = null
-	let closestOffset = Number.NEGATIVE_INFINITY
-
-	columns?.forEach((column) => {
-		const { right } = column.getBoundingClientRect()
-
-		const offset = mouseX - right
-
-		if (offset < 0 && offset > closestOffset) {
-			closestOffset = offset
-			closestTask = column
-		}
-	})
-
-	return closestTask
-}
 </script>
 
 <template>
@@ -70,10 +37,7 @@ const insertLeft = (mouseX: number) => {
 			class="w-full text-xl sm:text-2xl font-bold text-center border-b-2 border-border p-2 sm:p-3">
 			<h1>{{ projectStore.project?.name }}</h1>
 		</div>
-		<div
-			ref="board"
-			@dragover="dragOver"
-			class="w-full flex gap-4 p-2 overflow-x-auto">
+		<div ref="board" class="w-full flex gap-4 p-2 overflow-x-auto">
 			<KanbanColumn v-for="column in data" :key="column.id" :column="column">
 				<KanbanCard
 					v-for="task in column.tasks.filter(
