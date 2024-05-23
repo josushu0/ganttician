2c133582ccdd77c9c40533299f6b9a8b6ac3becf
Josué Martell <josue.martell17@gmail.com>
2024-05-22 19:00:52 -0600

    feat: created kanban card component

---
+++ components/KanbanCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { Tasks } from '~/db/schema/tasks'
+
+defineProps<{
+	task: Tasks
+}>()
+
+const dragging = ref(false)
+const dragStart = () => {
+	dragging.value = true
+}
+
+const dragEnd = () => {
+	dragging.value = false
+}
+</script>
+
+<template>
+	<div
+		draggable="true"
+		@dragstart="dragStart"
+		@dragend="dragEnd"
+		class="rounded border-2 border-border p-2 select-none"
+		:class="dragging && 'is_dragging'">
+		{{ $props.task.name }}
+	</div>
+</template>
+
+<style scoped>
+.is_dragging {
+	background-color: hsl(var(--primary));
+	color: hsl(var(--primary-foreground));
+	border-color: hsl(var(--primary));
+}
+</style>
