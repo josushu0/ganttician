51685f0c5b76cbe0d5083725d009c9b414e912d1
Josué Martell <josue.martell17@gmail.com>
2024-05-22 18:56:42 -0600

    feat: added schema for kanban columns

---
+++ db/schema/columns.ts
@@ -0,0 +1,19 @@
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { projects } from './projects'
+import { relations } from 'drizzle-orm'
+import { tasks } from './tasks'
+
+export const columns = sqliteTable('columns', {
+	id: text('id').primaryKey(),
+	name: text('name').notNull(),
+	position: integer('position').notNull(),
+	project: text('project_id')
+		.notNull()
+		.references(() => projects.id, { onDelete: 'cascade' }),
+})
+
+export const columnsRelations = relations(columns, ({ many }) => ({
+	tasks: many(tasks),
+}))
+
+export type Columns = typeof columns.$inferSelect
